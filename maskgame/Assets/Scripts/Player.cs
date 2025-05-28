b8cd6f53ca6cec94c5b8122e3952ae739ef76825
Neiswer <othelpsas@gmail.com>
2025-05-28 16:45:32 +0700

    movement
    нихуя не добавил

---
+++ maskgame/Assets/Scripts/Player.cs
@@ -0,0 +1,102 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static UnityEngine.Rendering.STP;
+
+public class Player : MonoBehaviour
+{
+    #region Components
+    [SerializeField] PlayerMovement configMove;
+    InputAction mouseLook, movementAction, jumpActions;
+    Rigidbody rb;
+    #endregion
+    #region Movement
+
+    #region Move
+    Vector3 move;
+    Vector3 moveInput;
+    #endregion
+
+    #region Jump
+    bool onGround;
+    Vector3 originRaycastJump;
+    public float distanceRaycastJump = 0.75f;
+    RaycastHit hitGroundOnRaycast;
+    int jumpCount;
+    const int maxJumps = 2;
+    #endregion
+
+    #endregion
+
+
+    #region CameraLook
+    public Transform cameraHolder;
+    private float xRotation = 0f;
+    Vector2 look;
+    #endregion
+
+   
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        jumpActions = InputSystem.actions.FindAction("Jump");
+        jumpActions.Enable();
+        mouseLook = InputSystem.actions.FindAction("Look");
+        movementAction = InputSystem.actions.FindAction("Move");
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void LateUpdate()
+    {
+        LookAround();
+    }
+    private void FixedUpdate()
+    {
+        Movement();
+        Jump();
+    }
+    void Movement()
+    {
+        if (movementAction.IsPressed())
+        {
+            moveInput = movementAction.ReadValue<Vector3>();
+            move = transform.forward * moveInput.y + transform.right * moveInput.x;
+            rb.MovePosition(rb.position + move * configMove.Speed * Time.fixedDeltaTime);
+        }
+    }
+    void LookAround(float mouseSens = 0.1f)
+    {
+        look = mouseLook.ReadValue<Vector2>() * mouseSens;
+        xRotation -= look.y;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        cameraHolder.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * look.x);
+        
+    }
+    void Jump()
+    {
+        originRaycastJump = new Vector3(transform.position.x, transform.position.y - (transform.localScale.y * .5f), transform.position.z);
+        if (Physics.Raycast(originRaycastJump, transform.TransformDirection(Vector3.down), out hitGroundOnRaycast, distanceRaycastJump))
+        {
+            Debug.DrawRay(originRaycastJump, transform.TransformDirection(Vector3.down) * distanceRaycastJump, Color.red);
+            onGround = true;
+        }
+        else
+        {
+            onGround = false;
+        }
+
+        if (jumpActions.IsPressed() && onGround)
+        {
+            onGround = false;
+            rb.AddForce(0, configMove.JumpDistance, 0, ForceMode.Impulse);
+        }
+    }
+
+}
---
+++ maskgame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerMovementConfig", menuName = "Scriptable Objects/Player/PlayerMovementConfig")]
+public class PlayerMovement : ScriptableObject
+{
+    [field: SerializeField] public float Speed { get; private set; } = 5f;
+    [field: SerializeField] public float SprintSpeed { get; private set; }  = 10f;
+    [field: SerializeField] public float JumpDistance { get; private set; } = 1.2f;
+}

a730825b87e038bf98faba5a136d70cf67fb7df1
Neiswer <othelpsas@gmail.com>
2025-05-29 00:43:40 +0700

    Sprint
    попытался сделать спринт мне лень дальше, я спать. мб седня разгребу

--- maskgame/Assets/Scripts/Player.cs
+++ maskgame/Assets/Scripts/Player.cs
@@ -6,14 +6,19 @@ public class Player : MonoBehaviour
 {
     #region Components
     [SerializeField] PlayerMovementCFG configMove;
-    InputAction mouseLook, movementAction, jumpActions;
+    InputAction mouseLook, movementAction, jumpActions , sprintAction;
     Rigidbody rb;
     #endregion
     #region Movement
 
     #region Move
+    public float currentSpeed,coefSpeed = 1.2f;
+
     Vector3 move;
     Vector2 moveInput;
+
+    float stamina;
+    const float staminaMax = 100f;
     #endregion
 
     #region Jump
@@ -43,15 +48,19 @@ public class Player : MonoBehaviour
         jumpActions = InputSystem.actions.FindAction("Jump");
         mouseLook = InputSystem.actions.FindAction("Look");
         movementAction = InputSystem.actions.FindAction("Move");
+        sprintAction = InputSystem.actions.FindAction("Sprint");
 
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+
+        currentSpeed = configMove.Speed;
+        stamina = staminaMax;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        Sprint();
     }
     private void LateUpdate()
     {
@@ -66,10 +75,38 @@ public class Player : MonoBehaviour
     {
         if (movementAction.IsPressed())
         {
+
             moveInput = movementAction.ReadValue<Vector2>();
             move = transform.forward * moveInput.y + transform.right * moveInput.x;
-            rb.MovePosition(rb.position + move * configMove.Speed * Time.fixedDeltaTime);
+
+            //Debug.Log(currentSpeed);    
+            rb.MovePosition(rb.position + move * currentSpeed * Time.fixedDeltaTime);
         }
+    }
+    void Sprint()
+    {
+        if (sprintAction.IsPressed() && stamina > 0)
+        {
+            currentSpeed += 15f * Time.deltaTime;
+            stamina -= 20f * Time.deltaTime;
+
+        }
+        else if(!sprintAction.IsPressed())
+        {
+            currentSpeed -= coefSpeed * Time.deltaTime;
+            if(stamina < staminaMax)
+            {
+                stamina -= 20f * Time.deltaTime;
+            }
+            
+        }
+        else if(stamina <= 0)
+        {
+
+        }
+
+        Debug.Log(stamina);
+        currentSpeed = Mathf.Clamp(currentSpeed, configMove.Speed, configMove.SprintSpeed);
     }
     void LookAround(float mouseSens = 0.1f)
     {

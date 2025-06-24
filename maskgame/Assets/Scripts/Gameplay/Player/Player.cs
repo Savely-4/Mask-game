8885adb8f5af57d93a1cfd9d4b4319623413ab02
Ilya Losev <112805568+Dengeatz@users.noreply.github.com>
2025-06-24 20:49:19 +0500

    Rework movement system

--- maskgame/Assets/Scripts/Gameplay/Player/Player.cs
+++ maskgame/Assets/Scripts/Gameplay/Player/Player.cs
@@ -7,70 +7,60 @@ using System.Collections;
 
 public class Player : MonoBehaviour
 {
-    #region Components
-    [SerializeField] PlayerMovementCFG configMove;
-    public static Player player;
+    //[SerializeField] public float Speed { get; private set; } = 5f;
+    //[SerializeField] public float SprintSpeed { get; private set; } = 10f;
+    //[SerializeField] public float JumpDistance { get; private set; } = 1.7f;
+
+    [SerializeField] private Camera _camera;
+    [SerializeField] private float _runSpeed;
+    [SerializeField] private float _walkSpeed;
+    [SerializeField] private float _lerpWalkSpeed;
+    [SerializeField] private float _lerpRunSpeed;
+    [SerializeField] private float _jumpDistance;
+
+
     public InputAction mouseLook, movementAction, jumpAction, sprintAction, dashAction,interact,mousePosAction;
-    Rigidbody rb;
-    HpOnObject hpPlayer;
-    Animator playerAnimator;
-    #endregion
-    #region Movement
-
-    #region Move
-    public float currentSpeed, coefSpeed = 4f;
-
-    Vector3 move;
-    Vector2 moveInput;
-
-    private Dictionary<string, bool> cooldowns = new();
-    float stamina;
-    const float staminaMax = 100f;
-    float staminaRegenDelayTimer = 0f;
-    bool playerCantSprint;
-    Vector3 directionalDash;
-    #endregion
-    #region Dash
-    public float distanceDash = 5f;
-    float timeToCdDash = 4f;
-    public float speedDash = 10f;
-    bool cdDash = false;
-
-    #endregion
-    #region Jump
-
-    bool onGround;
-    Vector3 originRaycastJump;
-    public float distanceRaycastJump = 0.75f;
-    RaycastHit hitGroundOnRaycast;
-    bool canDoubleJump;
-    bool canJump;
-    #endregion
-
-    #endregion
-    #region CameraLook
-    public Transform cameraHolder;
-    private float xRotation = 0f;
-    Vector2 look;
-    #endregion
-    #region Animation
-    bool nonAllEneract = false;
-
-    #endregion
-    #region BattleSystem
-    [SerializeField] private Weapon currentWeapon;
-    public Transform hands;
+    public float CoefSpeed = 4f;
+    public float DistanceDash = 5f;
+    public float SpeedDash = 10f;
+    public float DistanceRaycastJump = 0.75f;
+    public Transform CameraHolder;
+    public Transform Hands;
     public bool SwordInHands = false;
-    #endregion
+    
+    private CharacterController _characterController;
+    private Vector3 moveDirection;
+    private Vector2 moveInput;
+    private Dictionary<string, bool> cooldowns = new();
+    private float stamina;
+    private const float staminaMax = 100f;
+    private float staminaRegenDelayTimer = 0f;
+    private bool playerCantSprint;
+    private Vector3 directionalDash;
+    private float timeToCdDash = 4f;
+    private bool cdDash = false;
+    private bool onGround;
+    private Vector3 originRaycastJump;
+    private RaycastHit hitGroundOnRaycast;
+    private bool canDoubleJump;
+    private bool canJump;
+    private float xRotation = 0f;
+    private Vector2 look;
+    private bool notAllInteract = false;
 
+    private float _currentRunSpeed;
+    private float _currentWalkSpeed;
+    private float _currentSpeed;
+
+    private float _walkTime;
+    private float _sprintTime;
+    private float _lastRunSpeed;
+    private bool _isWasRunning;
+    private float _lastWalkSpeed;
 
     private void Awake()
     {
-        player = this;
-        playerAnimator = GetComponent<Animator>();
-        hpPlayer = GetComponent<HpOnObject>();
-        rb = GetComponent<Rigidbody>();
-        
+        _characterController = this.GetComponent<CharacterController>();    
         interact = InputSystem.actions.FindAction("Interact");
         dashAction = InputSystem.actions.FindAction("Dash");
         jumpAction = InputSystem.actions.FindAction("Jump");
@@ -78,144 +68,171 @@ public class Player : MonoBehaviour
         movementAction = InputSystem.actions.FindAction("Move");
         mousePosAction = InputSystem.actions.FindAction("MousePositions");
         sprintAction = InputSystem.actions.FindAction("Sprint");
-        
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
     }
     void Start()
     {
-        currentSpeed = configMove.Speed;
         stamina = staminaMax;
-        cameraHolder = Camera.main.transform;
-        hpPlayer.hp = 100f;
-        hpPlayer.maxHp = 100f;
-        hpPlayer.regenRate = 2f;
+        CameraHolder = _camera.transform;
     }
 
     void Update()
     {
-        if(interact.IsPressed() && SwordInHands)
-        {
-            currentWeapon = FindFirstObjectByType<Weapon>();
-            currentWeapon?.Attack();
-        }
-        if(Mouse.current.leftButton.IsPressed()) currentWeapon?.Attack();
+        _currentSpeed = _currentWalkSpeed + _currentRunSpeed;
+        //if(interact.IsPressed() && SwordInHands)
+        //{
+        //    currentWeapon = FindFirstObjectByType<Weapon>();
+        //    currentWeapon?.Attack();
+        //}
+        //if(Mouse.current.leftButton.IsPressed()) currentWeapon?.Attack();
 
-        Sprint();
-        Dash();
-        if (jumpAction.WasPressedThisFrame() && !nonAllEneract)
+        //Sprint();
+        //Dash();
+        Movement();
+        //Jump();
+        LookAround();
+
+        if (jumpAction.WasPressedThisFrame() && !notAllInteract)
             canJump = true;
     }
-    private void LateUpdate()
-    {
-        LookAround();
-    }
-    private void FixedUpdate()
-    {
-        Movement();
-        Jump();
-    }
+
     void Movement()
     {
-        if (movementAction.IsPressed() && !cdDash && !nonAllEneract)
+        if (movementAction.IsPressed() && !cdDash && !notAllInteract)
         {
             moveInput = movementAction.ReadValue<Vector2>();
-            move = transform.forward * moveInput.y + transform.right * moveInput.x;
-   
-            rb.MovePosition(rb.position + move * currentSpeed * Time.fixedDeltaTime);
-        }
-    }
-    void Dash()
-    {
-        if (IsOnCooldown("Dash") || nonAllEneract) return;
+            
+            if(moveInput.magnitude > 0.0f)
+            {
+                if (_walkTime < 1.0f)
+                    _walkTime += Time.deltaTime / _lerpWalkSpeed;
+                else
+                    _walkTime = 1.0f;
 
-        if (dashAction.IsPressed() && !cdDash && movementAction.IsPressed())
-        {
-            directionalDash = transform.position + new Vector3(moveInput.x,moveInput.y,0) * distanceDash;
-            directionalDash.y = transform.position.y;
-            Vector3 vector = Vector3.MoveTowards(transform.position, directionalDash, speedDash * Time.deltaTime);
-            rb.MovePosition(vector);
-            cdDash = true;
-        }
-        else if(!cdDash && dashAction.IsPressed())
-        {
-            directionalDash = transform.position + transform.forward * distanceDash;
-            directionalDash.y = transform.position.y;
-            Vector3 vector = Vector3.MoveTowards(transform.position, directionalDash, speedDash * Time.deltaTime);
-            rb.MovePosition(vector);
-            cdDash = true;
-        }
-        else if (cdDash)
-        {
-            StartCooldown("Dash", 4f);
-            cdDash = false;
-        }
-    }
-    void Sprint()
-    {
-        if (nonAllEneract) return;
+                _lastWalkSpeed = _currentWalkSpeed;
+                _currentWalkSpeed = Mathf.Lerp(_currentWalkSpeed, _walkSpeed, _walkTime);
+            } 
+            else
+            {
+                if (_walkTime > 0.0f)
+                    _walkTime -= Time.deltaTime / _lerpWalkSpeed;
+                else
+                    _walkTime = 0.0f;
 
-        if(stamina <= 0 && !IsOnCooldown("Sprint") && !playerCantSprint)
-        {
-            stamina = 0;
-            playerCantSprint = true;
-            StartCooldown("Spint", 5f);
-        }
-        if (playerCantSprint && stamina >= staminaMax * 0.3f) playerCantSprint = false;
-
-        if(sprintAction.IsPressed() && movementAction.IsPressed() && !IsOnCooldown("Sprint") && !playerCantSprint && stamina > 0)
-        {
-            stamina -= 20f * Time.deltaTime;
-
-            if (stamina < 0) stamina = 0;
-
-            if (currentSpeed < configMove.SprintSpeed) currentSpeed += coefSpeed * Time.deltaTime;
-            staminaRegenDelayTimer = 1.5f;
+                _currentWalkSpeed = Mathf.Lerp(0f, _lastWalkSpeed, _walkTime);
+            }
+            
+            moveDirection = transform.forward * moveInput.y + transform.right * moveInput.x;
+            
         }
         else
         {
-            if(staminaRegenDelayTimer > 0f) staminaRegenDelayTimer -= Time.deltaTime;
-            else if (stamina < staminaMax) stamina += 10f * Time.deltaTime;
+            if (_walkTime > 0.0f)
+                _walkTime -= Time.deltaTime / _lerpWalkSpeed;
+            else
+                _walkTime = 0.0f;
 
-            if(currentSpeed > configMove.Speed) currentSpeed -= coefSpeed * Time.deltaTime;
-
+            _currentWalkSpeed = Mathf.Lerp(0f, _lastWalkSpeed, _walkTime);
         }
-        currentSpeed = Mathf.Clamp(currentSpeed, configMove.Speed, configMove.SprintSpeed);
-        Mathf.Clamp(stamina, 0f, staminaMax);
+
+        _characterController.Move(_currentSpeed * Time.deltaTime * moveDirection.normalized + (this.transform.up * -9.81f));
+
     }
+
+    //void Dash()
+    //{
+    //    if (IsOnCooldown("Dash") || notAllInteract) return;
+
+    //    if (dashAction.IsPressed() && !cdDash && movementAction.IsPressed())
+    //    {
+    //        directionalDash = transform.position + new Vector3(moveInput.x,moveInput.y,0) * DistanceDash;
+    //        directionalDash.y = transform.position.y;
+    //        Vector3 vector = Vector3.MoveTowards(transform.position, directionalDash, SpeedDash * Time.deltaTime);
+    //        rb.MovePosition(vector);
+    //        cdDash = true;
+    //    }
+    //    else if(!cdDash && dashAction.IsPressed())
+    //    {
+    //        directionalDash = transform.position + transform.forward * DistanceDash;
+    //        directionalDash.y = transform.position.y;
+    //        Vector3 vector = Vector3.MoveTowards(transform.position, directionalDash, SpeedDash * Time.deltaTime);
+    //        rb.MovePosition(vector);
+    //        cdDash = true;
+    //    }
+    //    else if (cdDash)
+    //    {
+    //        StartCooldown("Dash", 4f);
+    //        cdDash = false;
+    //    }
+    //}
+
+    //void Sprint()
+    //{
+    //    if (notAllInteract) return;
+
+    //    if(stamina <= 0 && !IsOnCooldown("Sprint") && !playerCantSprint)
+    //    {
+    //        stamina = 0;
+    //        playerCantSprint = true;
+    //        StartCooldown("Spint", 5f);
+    //    }
+    //    if (playerCantSprint && stamina >= staminaMax * 0.3f) playerCantSprint = false;
+
+    //    if(sprintAction.IsPressed() && movementAction.IsPressed() && !IsOnCooldown("Sprint") && !playerCantSprint && stamina > 0)
+    //    {
+    //        stamina -= 20f * Time.deltaTime;
+
+    //        if (stamina < 0) stamina = 0;
+
+    //        if (CurrentSpeed < configMove.SprintSpeed) CurrentSpeed += CoefSpeed * Time.deltaTime;
+    //        staminaRegenDelayTimer = 1.5f;
+    //    }
+    //    else
+    //    {
+    //        if(staminaRegenDelayTimer > 0f) staminaRegenDelayTimer -= Time.deltaTime;
+    //        else if (stamina < staminaMax) stamina += 10f * Time.deltaTime;
+
+    //        if(CurrentSpeed > configMove.Speed) CurrentSpeed -= CoefSpeed * Time.deltaTime;
+
+    //    }
+    //    CurrentSpeed = Mathf.Clamp(CurrentSpeed, configMove.Speed, configMove.SprintSpeed);
+    //    Mathf.Clamp(stamina, 0f, staminaMax);
+    //}
     void LookAround(float mouseSens = 0.1f)
     {
-        if(nonAllEneract) return;
+        if(notAllInteract) return;
         look = mouseLook.ReadValue<Vector2>() * mouseSens;
         xRotation -= look.y;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-        cameraHolder.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        CameraHolder.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         transform.Rotate(Vector3.up * look.x);
     }
-    void Jump()
-    {
-        originRaycastJump = new Vector3(transform.position.x, transform.position.y - (transform.localScale.y * 0.5f), transform.position.z);
-        onGround = Physics.Raycast(originRaycastJump, Vector3.down, out hitGroundOnRaycast, distanceRaycastJump);
-        if (onGround) canDoubleJump = true;
+    //void Jump()
+    //{
+    //    originRaycastJump = new Vector3(transform.position.x, transform.position.y - (transform.localScale.y * 0.5f), transform.position.z);
+    //    onGround = Physics.Raycast(originRaycastJump, Vector3.down, out hitGroundOnRaycast, DistanceRaycastJump);
+    //    if (onGround) canDoubleJump = true;
 
-        if (!canJump) return;
+    //    if (!canJump) return;
 
-        if (onGround) rb.AddForce(0, configMove.JumpDistance, 0, ForceMode.Impulse);
+    //    if (onGround) rb.AddForce(0, configMove.JumpDistance, 0, ForceMode.Impulse);
 
-        else if (canDoubleJump)
-        {
-            rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
-            rb.AddForce(0, configMove.JumpDistance, 0, ForceMode.Impulse);
-            canDoubleJump = false;
-        }
-        canJump = false;
-    }
+    //    else if (canDoubleJump)
+    //    {
+    //        rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+    //        rb.AddForce(0, configMove.JumpDistance, 0, ForceMode.Impulse);
+    //        canDoubleJump = false;
+    //    }
+    //    canJump = false;
+    //}
+
     IEnumerator NonEnteract(int layer, float secondsToWait = 10)
     {
-        nonAllEneract = true;
+        notAllInteract = true;
         yield return new WaitForSeconds(secondsToWait);
-        nonAllEneract = false;
+        notAllInteract = false;
     }
 
     void TakeItem()
--- maskgame/Assets/Scripts/Gameplay/Player/PlayerMovementCFG.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "PlayerMovementConfig", menuName = "Scriptable Objects/Player/PlayerMovementConfig")]
-public class PlayerMovementCFG : ScriptableObject
-{
-    [field: SerializeField] public float Speed { get; private set; } = 5f;
-    [field: SerializeField] public float SprintSpeed { get; private set; }  = 10f;
-    [field: SerializeField] public float JumpDistance { get; private set; } = 1.7f;
-}

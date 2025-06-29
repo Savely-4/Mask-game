0e5419fc3c2e1fd338185f99c1bd0d268a91ecd0
TERAFlYs2 <tera.work222@gmail.com>
2025-06-29 13:57:46 +0300

    PlayerInputKeyboardSystem
    Adding empty files

---
+++ maskgame/Assets/Scripts/Gameplay/Player/PlayerInputKeyboard/PlayerInputKeyboardConfig.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerInputKeyboardConfig", menuName = "Scriptable Objects/PlayerInputKeyboardConfig")]
+public class PlayerInputKeyboardConfig : ScriptableObject
+{
+    
+}

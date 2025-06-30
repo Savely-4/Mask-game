d36725b2ee384d6f53df84b79b08ff0877c895c4
Augrider <alotodem@gmail.com>
2025-06-30 19:17:46 +0300

    Added Player Movement class
    Added Player Movement class and Player_New class which uses new movement

--- maskgame/Assets/Scripts/Gameplay/Player/Player_New.cs
+++ maskgame/Assets/Scripts/Gameplay/Player/Player_New.cs
@@ -5,62 +5,28 @@ using UnityEngine.InputSystem;
 using System.Threading.Tasks;
 using System.Collections;
 
-public class Player : MonoBehaviour
+public class Player_New : MonoBehaviour
 {
-    //[SerializeField] public float Speed { get; private set; } = 5f;
-    //[SerializeField] public float SprintSpeed { get; private set; } = 10f;
-    //[SerializeField] public float JumpDistance { get; private set; } = 1.7f;
+    [SerializeField] private PlayerMovement movement;
 
     [SerializeField] private Camera _camera;
-    [SerializeField] private float _runSpeed;
-    [SerializeField] private float _walkSpeed;
-    [SerializeField] private float _lerpWalkSpeed;
-    [SerializeField] private float _lerpRunSpeed;
-    [SerializeField] private float _jumpDistance;
-
 
     public InputAction mouseLook, movementAction, jumpAction, sprintAction, dashAction, interact, mousePosAction;
-    public float CoefSpeed = 4f;
-    public float DistanceDash = 5f;
-    public float SpeedDash = 10f;
-    public float DistanceRaycastJump = 0.75f;
     public Transform CameraHolder;
-    public Transform Hands;
-    public bool SwordInHands = false;
 
-    private CharacterController _characterController;
-    private Vector3 moveDirection;
     private Vector2 moveInput;
     private Dictionary<string, bool> cooldowns = new();
     private float stamina;
     private const float staminaMax = 100f;
-    private float staminaRegenDelayTimer = 0f;
-    private bool playerCantSprint;
-    private Vector3 directionalDash;
-    private float timeToCdDash = 4f;
     private bool cdDash = false;
-    private bool onGround;
-    private Vector3 originRaycastJump;
-    private RaycastHit hitGroundOnRaycast;
     private bool canDoubleJump;
-    private bool canJump;
     private float xRotation = 0f;
     private Vector2 look;
     private bool notAllInteract = false;
 
-    private float _currentRunSpeed;
-    private float _currentWalkSpeed;
-    private float _currentSpeed;
-
-    private float _walkTime;
-    private float _sprintTime;
-    private float _lastRunSpeed;
-    private bool _isWasRunning;
-    private float _lastWalkSpeed;
 
     private void Awake()
     {
-        _characterController = this.GetComponent<CharacterController>();
         interact = InputSystem.actions.FindAction("Interact");
         dashAction = InputSystem.actions.FindAction("Dash");
         jumpAction = InputSystem.actions.FindAction("Jump");
@@ -80,57 +46,28 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        _currentSpeed = _currentWalkSpeed + _currentRunSpeed;
         Movement();
-        LookAround();
 
         if (jumpAction.WasPressedThisFrame() && !notAllInteract)
-            canJump = true;
+            Jump();
+
+        LookAround();
     }
 
 
-
+    //TODO: Fix reversed input values
     void Movement()
     {
         if (movementAction.IsPressed() && !cdDash && !notAllInteract)
         {
             moveInput = movementAction.ReadValue<Vector2>();
-
-            if (moveInput.magnitude > 0.0f)
-            {
-                if (_walkTime < 1.0f)
-                    _walkTime += Time.deltaTime / _lerpWalkSpeed;
-                else
-                    _walkTime = 1.0f;
-
-                _lastWalkSpeed = _currentWalkSpeed;
-                _currentWalkSpeed = Mathf.Lerp(_currentWalkSpeed, _walkSpeed, _walkTime);
-            }
-            else
-            {
-                if (_walkTime > 0.0f)
-                    _walkTime -= Time.deltaTime / _lerpWalkSpeed;
-                else
-                    _walkTime = 0.0f;
-
-                _currentWalkSpeed = Mathf.Lerp(0f, _lastWalkSpeed, _walkTime);
-            }
-
-            moveDirection = transform.forward * moveInput.y + transform.right * moveInput.x;
-
+            moveInput = new Vector2(moveInput.y, moveInput.x);
+            movement.SetMovementInput(moveInput);
         }
         else
         {
-            if (_walkTime > 0.0f)
-                _walkTime -= Time.deltaTime / _lerpWalkSpeed;
-            else
-                _walkTime = 0.0f;
-
-            _currentWalkSpeed = Mathf.Lerp(0f, _lastWalkSpeed, _walkTime);
+            movement.SetMovementInput(Vector2.zero);
         }
-
-        _characterController.Move(_currentSpeed * Time.deltaTime * moveDirection.normalized + (this.transform.up * -9.81f));
-
     }
 
     //void Dash()
@@ -192,6 +129,7 @@ public class Player : MonoBehaviour
     //    CurrentSpeed = Mathf.Clamp(CurrentSpeed, configMove.Speed, configMove.SprintSpeed);
     //    Mathf.Clamp(stamina, 0f, staminaMax);
     //}
+
     void LookAround(float mouseSens = 0.1f)
     {
         if (notAllInteract) return;
@@ -202,24 +140,23 @@ public class Player : MonoBehaviour
         CameraHolder.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         transform.Rotate(Vector3.up * look.x);
     }
-    //void Jump()
-    //{
-    //    originRaycastJump = new Vector3(transform.position.x, transform.position.y - (transform.localScale.y * 0.5f), transform.position.z);
-    //    onGround = Physics.Raycast(originRaycastJump, Vector3.down, out hitGroundOnRaycast, DistanceRaycastJump);
-    //    if (onGround) canDoubleJump = true;
 
-    //    if (!canJump) return;
+    void Jump()
+    {
+        if (movement.IsGrounded)
+        {
+            movement.Jump();
+            canDoubleJump = true;
 
-    //    if (onGround) rb.AddForce(0, configMove.JumpDistance, 0, ForceMode.Impulse);
+            return;
+        }
 
-    //    else if (canDoubleJump)
-    //    {
-    //        rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
-    //        rb.AddForce(0, configMove.JumpDistance, 0, ForceMode.Impulse);
-    //        canDoubleJump = false;
-    //    }
-    //    canJump = false;
-    //}
+        if (!canDoubleJump)
+            return;
+
+        movement.Jump();
+        canDoubleJump = false;
+    }
 
     IEnumerator NonEnteract(int layer, float secondsToWait = 10)
     {
@@ -255,5 +192,4 @@ public class Player : MonoBehaviour
     }
 
     #endregion
-
 }

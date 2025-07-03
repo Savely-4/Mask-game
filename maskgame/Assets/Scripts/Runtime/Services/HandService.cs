18f26a39972d1cfb96f9ee8cd17610073a701710
Neiswer <othelpsas@gmail.com>
2025-07-03 18:11:37 +0700

    Feature HandSevice

--- maskgame/Assets/Scripts/Runtime/Services/HandService.cs
+++ maskgame/Assets/Scripts/Runtime/Services/HandService.cs
@@ -1,7 +1,17 @@
+using UnityEngine;
+
 namespace Runtime.Services
 {
     public class HandService
     {
         
+       void ChangePositionHands(Transform hands,Vector3 positionToChange)
+       {
+          hands.gameObject.transform.position = positionToChange;
+       }
+       void HandBobbing(float currentVelocity)
+       {
+            
+       }
     }
 }
